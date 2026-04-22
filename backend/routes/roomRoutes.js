a8d7349cd4cea1dcca9564ccbdac47c314727fe1
Gia Nghi Dang <nghi.dang159@gmail.com>
2026-04-21 19:32:42 -0500

    feat: build the movie matching algorithm

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { createRoom, getRoom } = require('../controllers/roomController');
-const { movieNight, submitVote } = require('../controllers/movieController');
+const { movieNight, submitVote, getResults } = require('../controllers/movieController');
 
 
 // create-room
@@ -15,5 +15,8 @@ router.post('/:roomID/start', movieNight);
 
 // submit-vote
 router.post('/:roomID/vote', submitVote);
+
+// get-results
+router.get('/:roomID/results', getResults);
 
 module.exports = router;
